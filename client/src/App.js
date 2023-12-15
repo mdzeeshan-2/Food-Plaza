bdbcf62608ed1d343aa8b55acd9c8d2b002c7e4f
Md Zeeshan <98448457+mdzeeshan-2@users.noreply.github.com>
2023-12-16 00:30:05 +0530

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,69 @@
+import { getAuth } from "firebase/auth";
+import { motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { fadeInOut } from "./animations";
+import { getAllCartItems, validateUserJWTToken } from "./api";
+import { Alert, MainLoader, CheckOutSuccess, UsersOrder, Footer} from "./components";
+import { app } from "./config/firebase.config";
+import { Dashboard, Login, Main } from "./containers";
+import { setCartItems } from "./context/actions/cartAction";
+import { setUserDetails } from "./context/actions/userActions";
+const App = () => {
+  const firebaseAuth = getAuth(app);
+  const [isLoading, setIsLoading] = useState(false);
+  const alert = useSelector((state) => state.alert);
+  const location = useLocation();
+  const isMainPage = location.pathname === "/";
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsLoading(true);
+    firebaseAuth.onAuthStateChanged((cred) => {
+      if (cred) {
+        cred.getIdToken().then((token) => {
+          validateUserJWTToken(token).then((data) => {
+            if (data) {
+              getAllCartItems(data.user_id).then((items) => {
+                console.log(items);
+                dispatch(setCartItems(items));
+              });
+            }
+            dispatch(setUserDetails(data));
+          });
+        });
+      }
+      setInterval(() => {
+        setIsLoading(false);
+      }, 3000);
+    });
+  }, []);
+
+  return (
+    <div className="w-screen min-h-screen h-auto flex flex-col items-center justify-center">
+      {isLoading && (
+        <motion.div
+          {...fadeInOut}
+          className="fixed z-50 inset-0 bg-lightOverlay backdrop-blur-md flex items-center justify-center w-full"
+        >
+          <MainLoader />
+        </motion.div>
+      )}
+      <Routes>
+        <Route path="/*" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard/*" element={<Dashboard />} />
+        <Route path="/checkout-success" element={<CheckOutSuccess />} />
+        <Route path="/user-orders" element={<UsersOrder />} />
+      </Routes>
+
+      {alert?.type && <Alert type={alert?.type} message={alert?.message} />}
+
+      {isMainPage && <Footer />}
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/utils/styles.js
@@ -0,0 +1,66 @@
+export const isActiveStyles =
+  " text-2xl text-red-700 font-semibold hover:text-red-700 px-4 py-2 duration-100 transition-all ease-in-out";
+
+export const isNotActiveStyles =
+  " text-xl text-textColor hover:text-red-700 duration-100 px-4 py-2 transition-all ease-in-out";
+
+export const statuses = [
+  { id: 1, title: "Drinks", category: "drinks" },
+  { id: 2, title: "Deserts", category: "deserts" },
+  { id: 3, title: "Fruits", category: "fruits" },
+  { id: 4, title: "Rice", category: "rice" },
+  { id: 5, title: "Curry", category: "curry" },
+  { id: 6, title: "Chinese", category: "chinese" },
+  { id: 7, title: "Bread", category: "bread" },
+];
+
+export const randomData = [
+  {
+    id: 1,
+    imageURL:
+      "https://i.pinimg.com/474x/1f/17/25/1f17259e21c35209e73b3506603341e6.jpg",
+    product_name: "Iceream",
+    product_category: "Chocolate & vanilla",
+    product_price: "23.00",
+  },
+  {
+    id: 2,
+    imageURL:
+      "https://i.pinimg.com/474x/d2/43/95/d24395570b3574a15b5464ae5ea6520e.jpg",
+    product_name: "Strawberries",
+    product_category: "Fresh Strawberries",
+    product_price: "18.00",
+  },
+  {
+    id: 3,
+    imageURL:
+      "https://i.pinimg.com/474x/f8/d2/fc/f8d2fcc374828c94a8e747ad31ccfa6c.jpg",
+    product_name: "Pine Apple",
+    product_category: "Juicy Apples",
+    product_price: "16.00",
+  },
+  {
+    id: 4,
+    imageURL:
+      "https://i.pinimg.com/474x/81/a2/57/81a257e15e478604d5061745633e3cec.jpg",
+    product_name: "Chicken Kebab",
+    product_category: "Mixed Kebab Plate",
+    product_price: "22.0",
+  },
+  {
+    id: 5,
+    imageURL:
+      "https://i.pinimg.com/474x/57/cd/dd/57cddd925ee9c23164c2cfb69faf0e92.jpg",
+    product_name: "Mojito",
+    product_category: "Cocktail Drinks",
+    product_price: "10.0",
+  },
+  {
+    id: 6,
+    imageURL:
+      "https://i.pinimg.com/474x/43/f4/23/43f423fa9454b50c21a5c2cd82f40c7d.jpg",
+    product_name: "Prawn Masala",
+    product_category: "Spicy Masala",
+    product_price: "20.0",
+  },
+];
